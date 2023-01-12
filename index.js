e64192cef02f7874f96b75262318241dc134ed4a
Darya Naji <daaryaa.naji@gmail.com>
2023-01-12 19:44:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+const meesage = document.getElementById("meesage");
+const errorm = document.getElementById("errorm");
+function submitData(userName, userEmail) {
+  const formData = {
+    name: userName,
+    email: userEmail,
+  };
+
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const meess = document.createElement("div");
+      meess.innerHTML = object.id;
+      meesage.appendChild(meess);
+    })
+    .catch(function (error) {
+      const errror = document.createElement("div");
+      errror.innerHTML = error.message;
+      errorm.appendChild(errror);
+    });
+}
